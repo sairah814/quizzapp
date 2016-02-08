ca5ab36f2ec59d502e393ba7b10e5390995cd268
sairah814 <sairah814@gmail.com>
2016-02-07 21:19:20 -0500

    Started JS

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    //quiz-landing functionality
+    //quiz page functionality
+    //results functionality
+
+
+    /*Declare global variables*/
+
+
+    /*Define functions*/
+
+
+    /*Do stuff triggered by events*/
+    $('#start').click(function () {
+        $('.quiz-landing').hide(); // add some animation!
+        $('.quiz').css('display', 'block');
+    });
+});
